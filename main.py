c8618fcd1e2bb4d8388adb8c09c30da2051b3b90
Baev Evgeniy <bayev71@inbox.ru>
2024-03-31 18:57:22 +0300

    paint

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+import turtle
 from turtle import *
-screensize(600,600)
-# bgpic("elka.png")
-# hideturtle()
+
+
+def draw_line(point1: list, point2):
+    penup()
+    goto(point1[0], point1[1])
+    pendown()
+    goto(point2[0], point2[1])
+    penup()
+
+
+Screen(), setup(800, 700)
+
 shape("turtle")
 penup()
+draw_line([-400, -250], [-100, -250])
+draw_line([-120, -250], [-120, 300])
+draw_line([-120, 300], [-300, 300])
+draw_line([-300, 300], [-300, 250])
+# goto([-300, 150])
+pendown()
+turtle.circle(-50)
+draw_line([-300, 150], [-300, 100])
+draw_line([-300, 100], [-225, 125])
+# goto([-300, 100])
+draw_line([-300, 100], [-375, 125])
+# goto([-300, 100])
+draw_line([-300, 100], [-300, 30])
+draw_line([-300, 30], [-375, -60])
+# goto([-300, 30])
+draw_line([-300, 30], [-225, -60])
 
-# украшаем елку
-goto(80,170)
-dot(40,"red")
-# ваш код тут...
-
-# _
 mainloop()
